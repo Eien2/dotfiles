b835bba575f0dcf4266f90fe94e5e825d1e3c72a
Eien2 <oleksiaksebastian32@gmail.com>
2025-05-14 19:07:41 +0200

    added cursorline plugin and configured it

--- nvim/.config/nvim/lua/eien/plugins/misc.lua
+++ nvim/.config/nvim/lua/eien/plugins/misc.lua
@@ -14,4 +14,23 @@ return {
     "LintaoAmons/cd-project.nvim",
     tag = "v0.9.1"
   },
+  {
+    "vyfor/cord.nvim",
+    build = ":Cord update"
+  },
+  {
+    "ya2s/nvim-cursorline",
+    config = function()
+      require("nvim-cursorline").setup {
+        cursorline = {
+          enable = false,
+        },
+        cursorword = {
+          enable = true,
+          min_length = 3,
+          hl = { underline = true },
+        }
+      }
+    end
+  },
 }
