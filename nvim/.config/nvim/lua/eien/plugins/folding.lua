e62c2c32406ebf5c5ac542b13b63154a066e3648
Eien2 <oleksiaksebastian32@gmail.com>
2025-05-16 23:39:56 +0200

    added nvim-ufo plugin

--- nvim/.config/nvim/lua/eien/plugins/folding.lua
+++ nvim/.config/nvim/lua/eien/plugins/folding.lua
@@ -1,2 +1,33 @@
 return {
+  {
+    "kevinhwang91/nvim-ufo",
+    dependencies = {
+      "kevinhwang91/promise-async",
+    },
+
+    config = function()
+      require("ufo").setup({
+        provider_selector = function(bufnr, filetype, buftype)
+          return { "treesitter", "indent" }
+        end
+      })
+    end,
+  },
+  {
+    "luukvbaal/statuscol.nvim",
+    config = function()
+      local builtin = require("statuscol.builtin")
+      require("statuscol").setup({
+        segments = {
+          { text = { "%s" },             click = "v:lua.ScSa" },
+          { text = { builtin.lnumfunc }, click = "v:lua.ScLa", },
+          {
+            text = { " ", builtin.foldfunc, " " },
+            condition = { builtin.not_empty, true, builtin.not_empty },
+            click = "v:lua.ScFa"
+          },
+        }
+      })
+    end,
+  }
 }
--- nvim/.config/nvim/lua/eien/plugins/options.lua
+++ nvim/.config/nvim/lua/eien/plugins/options.lua
@@ -1,2 +1,9 @@
 vim.opt.termguicolors = true
 vim.cmd("colorscheme lackluster-hack")
+
+-- folding
+vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
+vim.o.foldcolumn = "1"
+vim.o.foldlevel = 99
+vim.o.foldlevelstart = 99
+vim.o.foldenable = true
