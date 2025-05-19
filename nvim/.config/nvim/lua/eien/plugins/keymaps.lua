d282c1eb215777704cc04a93c9d60e402c484338
Eien2 <oleksiaksebastian32@gmail.com>
2025-05-19 23:48:43 +0200

    added keybindings for auto-sessions

--- nvim/.config/nvim/lua/eien/plugins/keymaps.lua
+++ nvim/.config/nvim/lua/eien/plugins/keymaps.lua
@@ -45,3 +45,7 @@ vim.keymap.set("n", "<leader>cs", ":Trouble symbols focus=false<cr>")
 vim.keymap.set("n", "<leader>cl", ":Trouble lsp toggle focus=false win.position=right<cr>")
 vim.keymap.set("n", "<leader>xL", ":Trouble loclist toggle<cr>")
 vim.keymap.set("n", "<leader>xQ", ":Trouble qflist toggle<cr>")
+
+-- auto-sessions
+vim.keymap.set("n", "<leader>wr", ":SessionRestore<cr>")
+vim.keymap.set("n", "<leader>ws", ":SessionSave<cr>")
