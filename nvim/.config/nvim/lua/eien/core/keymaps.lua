48ce64a461eef5a4999868d5736a77c3669fc6d0
Eien2 <oleksiaksebastian32@gmail.com>
2025-05-21 17:34:00 +0200

    added essential keybindings - neovim

--- nvim/.config/nvim/lua/eien/core/keymaps.lua
+++ nvim/.config/nvim/lua/eien/core/keymaps.lua
@@ -1,19 +1,26 @@
--- leader key
-vim.g.mapleader = " "
+local opts = { noremap = true, silent = true }
 
--- yank to clipboard
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
 vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
 
--- buffers
 vim.keymap.set("n", "<leader>x", ":bw<cr>")
 
--- navigation
 vim.keymap.set("n", "<C-k>", ":wincmd k<cr>")
 vim.keymap.set("n", "<C-j>", ":wincmd j<cr>")
 vim.keymap.set("n", "<C-h>", ":wincmd h<cr>")
 vim.keymap.set("n", "<C-l>", ":wincmd l<cr>")
 
--- lsp
-vim.keymap.set("n", "<leader>f", function()
-  vim.lsp.buf.format()
-end)
+vim.keymap.set("v", "J", ":m '>+1<cr>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<cr>gv=gv")
+
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+vim.keymap.set("v", "<", "<gv", opts)
+vim.keymap.set("v", ">", ">gv", opts)
+
+vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
+
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
