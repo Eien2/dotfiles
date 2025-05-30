2b874dfa8679fdc3b2cd3ff0b9bb867ceda0d500
Eien2 <oleksiaksebastian32@gmail.com>
2025-05-30 21:52:01 +0200

    changed cd-project to project.plugin

--- nvim/.config/nvim/lua/eien/plugins/cd-project.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "LintaoAmons/cd-project.nvim",
-  tag = "v0.11.0",
-  config = function()
-    vim.keymap.set("n", "<leader>cd", "<cmd>CdProject<cr>")
-    vim.keymap.set("n", "<leader>cm", "<cmd>CdProjectManualAdd<cr>")
-  end,
-}
