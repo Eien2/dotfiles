3bdb7efbca5da26f57c26323ffe0e4605209bc31
Eien2 <oleksiaksebastian32@gmail.com>
2025-07-03 16:50:27 +0200

    added formatting on save, fixed treesitter

--- nvim/.config/nvim/lua/eien/plugins/formatting.lua
+++ nvim/.config/nvim/lua/eien/plugins/formatting.lua
@@ -19,10 +19,10 @@ return {
 				cs = { "clang-format" },
 				c = { "clang-format" },
 			},
-			-- format_on_save = {
-			-- 	timeout_ms = 500,
-			-- 	lsp_format = "fallback",
-			-- },
+			format_on_save = {
+				timeout_ms = 500,
+				lsp_format = "fallback",
+			},
 		})
 	end,
 }
