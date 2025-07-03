e538be97dda7df88df6b1c46bf75e5e3f910ef9e
Eien2 <oleksiaksebastian32@gmail.com>
2025-07-03 18:34:01 +0200

    added treesitter indenting, moved lualine to the top

--- nvim/.config/nvim/lua/eien/plugins/lualine.lua
+++ nvim/.config/nvim/lua/eien/plugins/lualine.lua
@@ -1,19 +1,20 @@
 return {
-  'nvim-lualine/lualine.nvim',
-  dependencies = { 'nvim-tree/nvim-web-devicons' },
-  config = function()
-    require("lualine").setup {
-      options = {
-        theme = "lackluster",
-      },
-      sections = {
-        lualine_a = { 'mode' },
-        lualine_b = { 'branch', 'diff', 'diagnostics' },
-        lualine_c = { 'filename' },
-        lualine_x = { 'encoding', 'fileformat', 'filetype' },
-        lualine_y = { 'lsp_status' },
-        lualine_z = { 'location' }
-      },
-    }
-  end,
+	"nvim-lualine/lualine.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	config = function()
+		require("lualine").setup({
+			options = {
+				theme = "lackluster",
+			},
+			tabline = {
+				lualine_a = { "mode" },
+				lualine_b = { "branch", "diff", "diagnostics" },
+				lualine_c = { "filename" },
+				lualine_x = { "encoding", "fileformat", "filetype" },
+				lualine_y = { "lsp_status" },
+				lualine_z = { "location" },
+			},
+			sections = {},
+		})
+	end,
 }
--- nvim/.config/nvim/lua/eien/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/eien/plugins/treesitter.lua
@@ -17,6 +17,9 @@ return {
 			highlight = {
 				enable = true,
 			},
+			indent = {
+				enable = true,
+			},
 		})
 	end,
 }
